bcffdbb8ea5159c0c7231c906a0490fabd841bce
chenai1112 <chen.ai1112@gmail.com>
2023-02-01 00:22:30 +0800

    Update 0680.验证回文字符串IIValid Palindrome II.js

--- 0680.验证回文字符串IIValid Palindrome II.js
+++ 0680.验证回文字符串IIValid Palindrome II.js
@@ -1,3 +1,32 @@
+/**
+680. 验证回文串 II
+给你一个字符串 s，最多 可以从中删除一个字符。
+
+请你判断 s 是否能成为回文字符串：如果能，返回 true ；否则，返回 false 。
+
+ 
+
+示例 1：
+
+输入：s = "aba"
+输出：true
+示例 2：
+
+输入：s = "abca"
+输出：true
+解释：你可以删除字符 'c' 。
+示例 3：
+
+输入：s = "abc"
+输出：false
+ 
+
+提示：
+
+1 <= s.length <= 105
+s 由小写英文字母组成
+通过次数126,640提交次数315,925
+*/
 var validPalindrome = function (s) {
   function isPalindrome(left, right) {
     while (left < right) {
